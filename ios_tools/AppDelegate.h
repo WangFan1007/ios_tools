84b74d0619b914422ed1f68ccecf0b44a16f520b
Ffine <ffine@macdemacbook-pro.local>
2018-03-05 15:25:24 +0800

    Initial Commit

---
+++ ios_tools/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ios_tools
+//
+//  Created by Ffine on 2018/3/5.
+//  Copyright © 2018年 Ffine. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ios_tools/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ios_tools
+//
+//  Created by Ffine on 2018/3/5.
+//  Copyright © 2018年 Ffine. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
